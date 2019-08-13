31b210160665fb547ea5b99a3011a83c7f2a3dac
ltei <xatofr35@gmail.com>
2019-08-13 21:06:53 +0200

    update

---
+++ lauopencv/src/main/java/com/ltei/lauopencv/osgi/OpenCVInterface.java
@@ -0,0 +1,8 @@
+package com.ltei.lauopencv.osgi;
+
+/**
+ * Dummy interface to allow some integration testing within OSGi implementation.
+ */
+public interface OpenCVInterface
+{
+}
---
+++ lauviews/src/main/java/com/ltei/lauviews/ViewLoadingProgress.kt
@@ -0,0 +1,50 @@
+package com.ltei.lauviews
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.ProgressBar
+import com.ltei.ljubase.Logger
+import com.ltei.ljubase.interfaces.ILoadListener
+import java.util.concurrent.atomic.AtomicInteger
+
+class ViewLoadingProgress : ProgressBar, ILoadListener {
+
+    private val logger = Logger(this)
+
+    private val loadingCount = AtomicInteger(0)
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    init {
+        visibility = View.GONE
+    }
+
+    fun clearLoads() {
+        loadingCount.set(0)
+    }
+
+    override fun onStartLoad() {
+        logger.info("onStartLoad")
+        val count = loadingCount.incrementAndGet()
+        if (count == 1) post { visibility = View.VISIBLE }
+    }
+
+    override fun onStopLoad() {
+        logger.info("onStopLoad")
+        val count = synchronized(loadingCount) {
+            val count = loadingCount.decrementAndGet()
+            if (count < 0) {
+                loadingCount.set(0)
+                0
+            } else count
+        }
+        if (count == 0) {
+            post { visibility = View.GONE }
+        } else if (count < 0) {
+            throw IllegalStateException()
+        }
+    }
+
+}
